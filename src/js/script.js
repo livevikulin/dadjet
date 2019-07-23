04e48e2b95e299b10b6b5f1dd1f9858510c953ec
Виталя Викулин <livevikulin@gmail.com>
2019-07-23 16:43:05 +0700

    Доверстал слайдер, сверстал блок с преимуществами, начал верстать блок с карточками товара

--- src/js/script.js
+++ src/js/script.js
@@ -2,8 +2,24 @@ import "jquery";
 import "slick-slider";
 
 $(document).ready(function () {
-	$('.header-basket').click(function() {
-		console.log('Ку-ку');
+
+	//Инициализация слайдера на главной
+	$('.main-slider').slick({
+		dots: true,
+		arrows: false,
+		// autoplay: true,
+		// autoplaySpeed: 4000
 	});
-	$('.main-slider').slick();
+	
+	//Анимация текстового блока в слайде
+	$('.slick-active').find('.slide-box').addClass('slide-box__anim');
+	$('.main-slider').on('afterChange', function(){
+		if ($('.slick-slide').hasClass('slick-active')) {
+			$('.slide-box__anim').removeClass('slide-box__anim');
+			$('.slick-active').find('.slide-box').addClass('slide-box__anim');
+		} else {
+			$('.slide-box__anim').removeClass('slide-box__anim');
+		}
+	}); 
+		
 });
