9143b13b3caaad5daf7c9c8b04744fe4be9fccfa
Виталя Викулин <livevikulin@gmail.com>
2019-07-25 15:33:26 +0700

    Доверстал главную страницу, поправил ховеры, добавил стрелки слайдеру, сделал модалку для авторизации/регистрации

--- src/js/script.js
+++ src/js/script.js
@@ -1,4 +1,7 @@
 import "jquery";
+import "@fancyapps/fancybox";
+import "jquery-ui";
+import "jquery-ui-bundle";
 import "slick-slider";
 
 $(document).ready(function () {
@@ -6,9 +9,9 @@ $(document).ready(function () {
 	//Инициализация слайдера на главной
 	$('.main-slider').slick({
 		dots: true,
-		arrows: false,
-		autoplay: true,
-		autoplaySpeed: 4000
+		arrows: true,
+		// autoplay: true,
+		// autoplaySpeed: 4000
 	});
 	
 	//Инициализация слайдера с партнерами
@@ -19,6 +22,37 @@ $(document).ready(function () {
 		variableWidth: true,
 		// autoplay: true,
 		// autoplaySpeed: 4000
+	});
+	
+	//Инициализация табов
+	$('#tabs').tabs({
+		hide: { 
+			effect: "fade", 
+			duration: 500 
+		},
+		show: { 
+			effect: "fade", 
+			duration: 500 
+		}
+	});
+	
+	$('#login-tabs').tabs({
+		hide: { 
+			effect: "fade", 
+			duration: 300 
+		},
+		show: { 
+			effect: "fade", 
+			duration: 300 
+		}
+	});
+	
+	//Инициализация fancybox
+	$('[data-fancybox]').fancybox({
+		helpers : {
+            media : {}
+        },
+        speed : 400,
 	});
 	
 	//Анимация текстового блока в слайде
@@ -32,4 +66,22 @@ $(document).ready(function () {
 		}
 	}); 
 		
+	//Активация кнопки "Получить КОД"
+	$('#reg_phone').on('change', function() {
+		if ($(this).val() != '') {
+			$('.login-phone__submit').removeClass('login-phone__none');
+		} else {
+			$('.login-phone__submit').addClass('login-phone__none');
+		}
+	})
+	
+	//Убираем лэйбл, если инпут заполнен
+	$('#input_user, #input_pass, #reg_name, #reg_phone, #reg_message, #reg_repeatPass').on('change', function() {
+		if ($(this).val() != '') {
+			$(this).next().css('display', 'none')
+		} else {
+			$(this).next().css('display', 'block')
+		}
+	});
+	
 });
