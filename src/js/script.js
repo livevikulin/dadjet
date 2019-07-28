f56efc17dae47b206069307267c55c57be7ff9c3
Виталий Викулин <livevikulin@gmail.com>
2019-07-29 02:18:27 +0700

    Сверстал корзину и страницу благодарности, остался адаптив

--- src/js/script.js
+++ src/js/script.js
@@ -76,6 +76,18 @@ $(document).ready(function () {
         },
         speed : 400,
 	});
+
+	//Анимация прокрутки страницы
+	$(".navbar-menu").on("click","a", function (event) {
+		//отменяем стандартную обработку нажатия по ссылке
+		event.preventDefault();
+		//забираем идентификатор бока с атрибута href
+		var id  = $(this).attr('href'),
+		//узнаем высоту от начала страницы до блока на который ссылается якорь
+		top = $(id).offset().top;
+		//анимируем переход на расстояние - top за 1500 мс
+		$('body,html').animate({scrollTop: top}, 700);
+	});
 	
 	//Анимация текстового блока в слайде
 	$('.slick-active').find('.slide-box').addClass('slide-box__anim');
@@ -98,7 +110,7 @@ $(document).ready(function () {
 	})
 	
 	//Убираем лэйбл, если инпут заполнен
-	$('#input_user, #input_pass, #reg_name, #reg_phone, #reg_message, #reg_pass, #reg_repeatPass, #feedback_name, #feedback_phone, #feedback_mail, #feedback_textarea').on('change', function() {
+	$('#input_user, #input_pass, #reg_name, #reg_phone, #reg_message, #reg_pass, #reg_repeatPass, #feedback_name, #feedback_phone, #feedback_mail, #feedback_textarea, #textarea_comment, #input_address, #input_pass, #input_e, #input_p, #input_o, #input_n, #input_f').on('change', function() {
 		if ($(this).val() != '') {
 			$(this).next().css('display', 'none')
 		} else {
@@ -106,4 +118,19 @@ $(document).ready(function () {
 		}
 	});
 	
+	//Счетчик для инпута
+	$('.basket-availability__minus').click(function () {
+		var $input = $(this).parent().find('input');
+		var count = parseInt($input.val()) - 1;
+		count = count < 0 ? 0 : count;
+		$input.val(count);
+		$input.change();
+		return false;
+	});
+	$('.basket-availability__plus').click(function () {
+		var $input = $(this).parent().find('input');
+		$input.val(parseInt($input.val()) + 1);
+		$input.change();
+		return false;
+	});
 });
