9d81d4d6398b82ea43a13b5f7f586bc34e5ae46f
Виталя Викулин <livevikulin@gmail.com>
2019-08-28 13:53:29 +0700

    поправил баг с сылками в меню

--- src/js/script.js
+++ src/js/script.js
@@ -172,8 +172,6 @@ $(document).ready(function () {
 
 	//Анимация прокрутки страницы
 	$(".navbar-menu, .mobile-menu").on("click", "a", function (event) {
-		//отменяем стандартную обработку нажатия по ссылке
-		event.preventDefault();
 		//забираем идентификатор бока с атрибута href
 		var id = $(this).attr("href"),
 			//узнаем высоту от начала страницы до блока на который ссылается якорь
