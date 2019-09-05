defb30fdd616ea9356f4b9fe3319bc5ad55bf477
Виталя Викулин <livevikulin@gmail.com>
2019-09-05 16:53:00 +0700

    задачи 55, 60, 64 поправлены

--- src/js/script.js
+++ src/js/script.js
@@ -46,13 +46,7 @@ $(document).ready(function () {
 		]
 	});
 
-	//Инициализация слайдера в карточке с товаром
-	
-	$('.product-slider__nav').on('afterChange', function (event, slick, currentSlide, nextSlide) {
-		let i = (currentSlide ? currentSlide : 0) + 1;
-		$('.js-modal-slider').slickGoTo(i);
-	});
-	
+	//Инициализация слайдера в карточке с товаром	
 	$(".product-slider__for").slick({
 		infinite: false,
 		slidesToShow: 1,
@@ -210,7 +204,7 @@ $(document).ready(function () {
 	});
 
 	//Убираем лэйбл, если инпут заполнен
-	const $inputs = $("#input_user, #input_pass, #reg_name, #reg_phone, #reg_message, #reg_pass, #reg_repeatPass, #feedback_name, #feedback_phone, #feedback_mail, #feedback_textarea, #textarea_comment, #input_address, #input_pass, #input_e, #input_p, #input_o, #input_n, #input_f, .form-control, [name='name'], [name='phone']");
+	const $inputs = $("#input_user, #input_pass, #reg_name, #reg_phone, #reg_message, #reg_pass, #reg_repeatPass, #feedback_name, #feedback_phone, #feedback_mail, #feedback_textarea, #textarea_comment, #input_address, #input_pass, #input_e, #input_p, #input_o, #input_n, #input_f, .form-control, [name='name'], [name='phone'], .js-lawyer");
 	
 	$inputs.each( (i, el) => {
 		const $el = $(el);
