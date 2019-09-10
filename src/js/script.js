8805b7fec34f7a9775e3a20063fcb26d7351570e
Виталя Викулин <livevikulin@gmail.com>
2019-09-10 10:09:47 +0700

    поправил мобильное меню

--- src/js/script.js
+++ src/js/script.js
@@ -180,7 +180,7 @@ $(document).ready(function () {
 
 
 	//Анимация прокрутки страницы
-	$(".navbar-menu, .mobile-menu, .navbar-fixed").on("click", "a", function (event) {
+	$(".navbar-menu, .navbar-fixed").on("click", "a", function (event) {
 		//забираем идентификатор бока с атрибута href
 		$('.navbar-fixed').addClass('nav-active');
 		var id = $(this).attr("href"),
@@ -188,6 +188,16 @@ $(document).ready(function () {
 			top = $(id).offset().top;
 		//анимируем переход на расстояние - top за 1500 мс
 		$("body,html").animate({scrollTop: top - 60}, 700);
+	});
+	
+	$(".mobile-menu a").on("click", function (event) {
+		//забираем идентификатор бока с атрибута href
+		$('.navbar-fixed').addClass('nav-active');
+		var id = $(this).attr("href"),
+			//узнаем высоту от начала страницы до блока на который ссылается якорь
+			top = $(id).offset().top;
+		//анимируем переход на расстояние - top за 1500 мс
+		$("body,html").animate({scrollTop: top - 115}, 700);
 	});
 
 	//Анимация текстового блока в слайде
